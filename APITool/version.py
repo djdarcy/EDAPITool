b4c7c91659e7c2c61c816001055d47efea1ca235
djdarcy <6962246+djdarcy@users.noreply.github.com>
2026-01-06 03:15:08 -0500

    Restore private-only files after merge

--- APITool/version.py
+++ APITool/version.py
@@ -26,7 +26,7 @@ PHASE = None  # First working release
 # Full version string - updated by git pre-commit hook
 # DO NOT EDIT THIS LINE MANUALLY
 # Note: Hash reflects the commit this version builds upon (HEAD at commit time)
-__version__ = "0.2.0_main_10-20260106-ddb1e99e"
+__version__ = "0.2.0_private_13-20260106-6338141f"
 
 
 def get_version():
